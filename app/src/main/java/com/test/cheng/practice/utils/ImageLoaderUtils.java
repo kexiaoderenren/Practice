b7fe2d9c7d68cff5d96d20e244a0b5ddb3ed5098
gaokuncheng <2450600200@qq.com>
2017-01-17 18:19:52 +0800

    commit

--- app/src/main/java/com/test/cheng/practice/utils/ImageLoaderUtils.java
+++ app/src/main/java/com/test/cheng/practice/utils/ImageLoaderUtils.java
@@ -1,14 +1,18 @@
 package com.test.cheng.practice.utils;
 
+import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
-import android.support.v4.app.Fragment;
+import android.graphics.Bitmap;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.util.Util;
 import com.test.cheng.practice.App;
+import com.test.cheng.practice.R;
 
 /**
  * 对Glide图片加载框架进行简单的封装
@@ -16,16 +20,59 @@ import com.test.cheng.practice.App;
  */
 public class ImageLoaderUtils {
 
+    /**
+     * 默认加载网络图片
+     * @param context
+     * @param url
+     * @param imageView
+     */
     public static void loadImg(Context context, String url, ImageView imageView) {
         if (Util.isOnMainThread()) {
-            Glide.with(context)
-                    .load(url)
-                    .centerCrop()
+            Glide.with(context).load(url)
+                    .placeholder(R.drawable.ic_default_color)
+                    .error(R.drawable.ic_default_color)
+                     .centerCrop()
                     .skipMemoryCache(true)
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .into(imageView);
         }
     }
+
+    /**
+     * Glide加载可伸缩图片
+     * @param context
+     * @param url
+     * @param imageView
+     */
+    public static void loadFlexibleImg(Context context, String url, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            Glide.with(context).load(url)
+                    .placeholder(R.drawable.ic_default_color)
+                    .error(R.drawable.ic_default_color)
+                    .skipMemoryCache(true)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(imageView);
+        }
+    }
+
+    public static void loadImgFitCenter(final Activity activity, final String url, final ImageView imageView, final int width) {
+         Glide.with(activity)
+                 .load(url)
+                 .asBitmap()
+                 .into(new SimpleTarget<Bitmap>() {
+                     @Override
+                     public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                         int imageWidth = resource.getWidth();
+                         int imageHeight = resource.getHeight();
+                         int height = width * imageHeight / imageWidth;
+                         ViewGroup.LayoutParams para = imageView.getLayoutParams();
+                         para.height = height;
+                         imageView.setLayoutParams(para);
+                         Glide.with(activity).load(url).asBitmap().into(imageView);
+                     }
+                 });
+    }
+
 
     /**
      * 暂停所有正在加载的图片
--- app/src/main/java/com/test/cheng/practice/view/common/PhotoViewActivity.java
+++ app/src/main/java/com/test/cheng/practice/view/common/PhotoViewActivity.java
@@ -1,14 +1,12 @@
 package com.test.cheng.practice.view.common;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Log;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import com.test.cheng.practice.R;
 import com.test.cheng.practice.utils.Constants;
@@ -28,7 +26,6 @@ import uk.co.senab.photoview.PhotoView;
 public class PhotoViewActivity extends BaseActivity {
 
     @BindView(R.id.photoview) PhotoView photoview;
-
     private String url;
 
     public static void start(Context context, String url) {
@@ -42,30 +39,25 @@ public class PhotoViewActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photoview);
         ButterKnife.bind(this);
-
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) photoview.getLayoutParams();
-        params.width = DeviceUtils.getSreenWidth(this);
-        photoview.setLayoutParams(params);
-
         if (savedInstanceState != null && !TextUtils.isEmpty(savedInstanceState.getString(Constants.PARAM_URL))) {
-            Log.d("test","--------- not null");
-            url = getIntent().getStringExtra(Constants.PARAM_URL);
+            url = savedInstanceState.getString(Constants.PARAM_URL);
         } else {
-            Log.d("test","--------- null");
             url = getIntent().getStringExtra(Constants.PARAM_URL);
         }
-        ImageLoaderUtils.loadImg(this, url, photoview);
+        photoview.setZoomable(true);
+        ImageLoaderUtils.loadFlexibleImg(this, url, photoview);
     }
 
-    @OnClick(R.id.img_download)
-    void downLoadImg() {
-        LogUtils.d("7777777777777777777777777777");
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(Constants.PARAM_TITLE, url);
+        outState.putString(Constants.PARAM_URL, url);
+        Log.i("test","------onSaveInstanceState()");
     }
 
     @Override
